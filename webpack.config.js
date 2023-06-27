fca3dc255b2a8d0516af0aeb82c6d58034201187
Michal <maxpoz3212@gmail.com>
2023-06-27 17:50:33 +0200

    Update Readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,17 @@
 const path = require('path');
-// importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// importuję odpowiedni plugin
 module.exports = {
   entry: "./src/index.js",
-  // definiuje plik wejściowy
   output: {
     path: path.resolve(__dirname, "build"),
-    // definiuje ścieżką wyjściową
     filename: "index.min.js",
-    // definiuję nazwę pliku wyjściowego
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        // określam jakie pliki
-        // będą brane pod uwagę
         exclude: /node_modules/,
-        // określam wykluczenia
         use: "babel-loader",
-        // określam jaki [loader]
-        // ma być wykorzystany
       },
       {
         test: /\.(ttf|otf|woff|woff2)$/,
@@ -31,15 +21,11 @@ module.exports = {
         },
       },
     ],
-    // obecnie brak dodatkowych ustawień
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      // wskazuje plik źródłowy
       filename: "index.html",
-      // określan nazwę dla pliku
     }),
   ],
 };
-// eksportuję ustawienia dla webpacka
